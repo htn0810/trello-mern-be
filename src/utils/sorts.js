63302efa0d21a3a2323da29135eba0f5d460323f
htn0810 <htn08102001@gmail.com>
2025-02-22 19:26:57 +0700

    init project

---
+++ src/utils/sorts.js
@@ -0,0 +1,11 @@
+// ee
+export const mapOrder = (originalArray, orderArray, key) => {
+  if (!originalArray || !orderArray || !key) return [];
+
+  const clonedArray = [...originalArray];
+  const orderedArray = clonedArray.sort((a, b) => {
+    return orderArray.indexOf(a[key]) - orderArray.indexOf(b[key]);
+  });
+
+  return orderedArray;
+};
